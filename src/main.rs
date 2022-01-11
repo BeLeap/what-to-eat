86ecd80ad58ea3ca85eb6666f88c451f08a58cf2
Changseo Jang <changseo.jang@korea.ac.kr>
2022-01-11 19:35:10 +0900

    basic client with empty handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::{self, client::EventHandler, Client};
+use std::env;
+
+struct Handler;
+
+#[serenity::async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let application_id: u64 = env::var("APPLICATION_ID")
+        .expect("Expected an application id in the environment")
+        .parse()
+        .expect("application id is not a valid id");
+
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .application_id(application_id)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
